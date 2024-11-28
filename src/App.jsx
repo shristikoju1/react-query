22fb40ad7be23a372a7bba7160113636b5f6524b
Shristi Koju <kojushristi4@gmail.com>
2024-11-28 14:46:20 +0545

    basic examples of tanstack react query

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+
+function App() {
+
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title })
+      )
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+    },
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
+  return (
+    <>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      )) || <h2>No Posts Available</h2>}
+      <button 
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}>Add New</button>
+    </>
+  );
+}
+
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
